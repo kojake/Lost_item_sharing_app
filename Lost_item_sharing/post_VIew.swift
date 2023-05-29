bf859e1db48a2c71f66830b214441f4888c4413c
kaito <ahonamame@gmail.com>
2023-05-29 21:11:53 +0900

    update

--- Lost_item_sharing/post_VIew.swift
+++ Lost_item_sharing/post_VIew.swift
@@ -18,72 +18,124 @@ struct post_VIew: View {
     @State private var content = ""
     
     var body: some View {
-        VStack{
-            GeometryReader { geometry in
-                Picker("", selection: self.$selectedIndex) {
-                    ForEach(0..<self.stapleFoods.count) { index in
-                        Text(self.stapleFoods[index])
-                            .tag(index)
-                    }
-                }
-                .pickerStyle(SegmentedPickerStyle())
-                .frame(width: 380)
-            }
+        NavigationView{
             VStack{
-                TextField("タップしてタイトルを入力", text: $title).frame(width: UIScreen.main.bounds.size.width - 80, height: 41, alignment: .center)
-                    .textFieldStyle(.plain)
-                    .background(Color.init(red: 211.0/255.0, green: 211.0/255.0, blue: 211.0/255.0))
-                    .textContentType(.emailAddress)
-                    .cornerRadius(10)
-                    .multilineTextAlignment(.center)
-                    .padding(.top, 15)
-                    .padding(.bottom, 10)
-                    .foregroundColor(.blue)
-                TextEditor(text: $content)
-                    .frame(height: 400)
-                    .padding()
-                    .border(Color.gray, width: 1)
-                Spacer()
-            }
-            HStack{
-                VStack{
-                    Text("実際の画像").fontWeight(.black)
-                    Image("")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 200, height: 180)
-                        .cornerRadius(0)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 4))
-                }
-                VStack{
-                    Button(action: {
-                        
-                    }) {
-                        Image(systemName: "camera.fill")
-                            .resizable()
-                            .padding()
-                            .frame(width: 80, height: 80)
-                            .imageScale(.large)
-                            .foregroundColor(Color.white)
-                            .background(Color.green)
-                            .clipShape(Circle())
+                GeometryReader { geometry in
+                    Picker("", selection: self.$selectedIndex) {
+                        ForEach(0..<self.stapleFoods.count) { index in
+                            Text(self.stapleFoods[index])
+                                .tag(index)
+                        }
                     }
-                    Button(action: {
-                        
-                    }) {
-                        Image(systemName: "photo")
-                            .resizable()
+                    .pickerStyle(SegmentedPickerStyle())
+                    .frame(width: 380)
+                }
+                if selectedIndex == 0{
+                    VStack{
+                        TextField("タップしてタイトルを入力", text: $title).frame(width: UIScreen.main.bounds.size.width - 80, height: 41, alignment: .center)
+                            .textFieldStyle(.plain)
+                            .background(Color.init(red: 211.0/255.0, green: 211.0/255.0, blue: 211.0/255.0))
+                            .textContentType(.emailAddress)
+                            .cornerRadius(10)
+                            .multilineTextAlignment(.center)
+                            .padding(.top, 15)
+                            .padding(.bottom, 10)
+                            .foregroundColor(.black)
+                            .fontWeight(.black)
+                        TextEditor(text: $content)
+                            .frame(height: 400)
                             .padding()
-                            .frame(width: 80, height: 80)
-                            .imageScale(.large)
-                            .foregroundColor(Color.white)
-                            .background(Color.green)
-                            .clipShape(Circle())
+                            .border(Color.gray, width: 1)
+                        Spacer()
+                    }
+                    HStack{
+                        VStack{
+                            Text("実際の画像").fontWeight(.black)
+                            Image("")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 200, height: 180)
+                                .cornerRadius(0)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 4))
+                        }
+                        VStack{
+                            Button(action: {
+                                
+                            }) {
+                                Image(systemName: "camera.fill")
+                                    .resizable()
+                                    .padding()
+                                    .frame(width: 80, height: 80)
+                                    .imageScale(.large)
+                                    .foregroundColor(Color.white)
+                                    .background(Color.green)
+                                    .clipShape(Circle())
+                            }
+                            Button(action: {
+                                
+                            }) {
+                                Image(systemName: "photo")
+                                    .resizable()
+                                    .padding()
+                                    .frame(width: 80, height: 80)
+                                    .imageScale(.large)
+                                    .foregroundColor(Color.white)
+                                    .background(Color.green)
+                                    .clipShape(Circle())
+                            }
+                        }
+                    }
+                }
+                else{
+                    VStack{
+                        Text("\(title)").font(.system(size: 50)).fontWeight(.black)
+                        VStack{
+                            Text("\(content)").font(.system(size: 20)).fontWeight(.black)
+                        }
+                        .frame(width: 350, height: 400)
+                        .padding()
+                        .border(Color.gray, width: 1)
+                        HStack{
+                            VStack{
+                                Text("実際の画像").fontWeight(.black)
+                                Image("")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 200, height: 180)
+                                    .cornerRadius(0)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 4))
+                            }
+                            Button(action: {
+                                
+                            }) {
+                                VStack {
+                                    Image(systemName: "message.fill")
+                                    Text("チャットを\n見てみる")
+                                }
+                            }.buttonStyle(BlueButtonStyle()).frame(width: 160, height: 100)
+                        }
+                        Spacer()
                     }
                 }
             }
         }
+    }
+}
+
+struct BlueButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.system(size: 20, weight:.bold, design: .rounded))
+            .foregroundColor(.white)
+            .padding(.horizontal)
+            .padding(5)
+            .background(Color.blue.opacity(0.8))
+            .cornerRadius(20)
+            .shadow(color:.black, radius: 4)
+            .opacity(configuration.isPressed ? 0.6 : 1.0)
+            .scaleEffect(configuration.isPressed ? 0.8 : 1.0)
     }
 }
 
