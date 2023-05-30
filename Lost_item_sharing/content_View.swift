36d4e184ca870e6c22d16b53f02ecdd7f7be93cc
kaito <ahonamame@gmail.com>
2023-05-30 15:25:04 +0900

    update

--- Lost_item_sharing/content_View.swift
+++ Lost_item_sharing/content_View.swift
@@ -20,6 +20,26 @@ struct content_View: View {
             .frame(width: 350, height: 400)
             .padding()
             .border(Color.gray, width: 1)
+            HStack{
+                VStack{
+                    Text("実際の画像").fontWeight(.black)
+                    Image("")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 200, height: 180)
+                        .cornerRadius(0)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 4))
+                }
+                Button(action: {
+                    
+                }) {
+                    VStack {
+                        Image(systemName: "message.fill")
+                        Text("チャットを\n見てみる")
+                    }
+                }.buttonStyle(BlueButtonStyle()).frame(width: 160, height: 100)
+            }
         }.onAppear{
             if let index = post_name.firstIndex(of: select_list){
                 selected_get_number = index
