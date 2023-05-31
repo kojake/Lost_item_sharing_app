01a7e433a123c8c6092ca8b742946e995e8ae456
kaito <ahonamame@gmail.com>
2023-05-31 19:40:30 +0900

    update

--- Lost_item_sharing/tag_change_View.swift
+++ Lost_item_sharing/tag_change_View.swift
@@ -11,8 +11,24 @@ struct tag_change_View: View {
     @Binding var lost_tag_list: [String]
     
     var body: some View {
-        VStack{
-            Text("タグを変更").font(.system(size: 50))
+        NavigationView{
+            ZStack{
+                Color.brown.ignoresSafeArea()
+                VStack{
+                    HStack{
+                        Image(systemName: "tag.fill").font(.largeTitle).fontWeight(.black)
+                        Text("タグを変更").font(.system(size: 50)).fontWeight(.black)
+                    }
+                    Text("現在使用されているタグ").font(.title).fontWeight(.black)
+                    VStack{
+                        HStack{
+                            Text("\(lost_tag_list[0])").frame(width: 100, height: 70).font(.title).fontWeight(.black).background(Color.green).cornerRadius(20)
+                            Text("\(lost_tag_list[1])").frame(width: 100, height: 70).font(.title).fontWeight(.black).background(Color.green).cornerRadius(20)
+                        }
+                    }.frame(width: 350, height: 100).background(Color.white).cornerRadius(20)
+                }
+            }
         }
     }
 }
+
