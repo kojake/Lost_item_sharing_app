1d757e70df9bf52cd394e0f69abd641b904bed47
kaito <ahonamame@gmail.com>
2023-06-01 09:56:11 +0900

    update

--- Lost_item_sharing/content_View.swift
+++ Lost_item_sharing/content_View.swift
@@ -19,126 +19,58 @@ struct content_View: View {
     
     var body: some View {
         NavigationView{
-            // 画面サイズの取得にGeometoryReaderを利用します
-            GeometryReader { geometry in
-                ZStack(alignment: .leading) {
-                    // メインコンテンツ
-                    VStack{
-                        HStack{
-                            Button(action: {
-                                dismiss()
-                            }) {
-                                Text("戻る")
-                            }
-                            .padding()
-                            .accentColor(Color.white)
-                            .background(Color.blue)
-                            .cornerRadius(26)
-                            Spacer()
-                        }
-                        Spacer()
-                        Text("\(post_title[selected_get_number])").font(.system(size: 50)).fontWeight(.black)
-                        VStack{
-                            Text("\(post_content[selected_get_number])").font(.system(size: 20)).fontWeight(.black)
-                        }
-                        .frame(width: 350, height: 400)
-                        .padding()
-                        .border(Color.gray, width: 1)
-                        HStack{
-                            VStack{
-                                Text("実際の画像").fontWeight(.black)
-                                Image("")
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(width: 200, height: 180)
-                                    .cornerRadius(0)
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 4))
-                            }
-                            Button(action: {
-                                if (self.offset == self.openOffset) {
-                                    self.offset = self.closeOffset
-                                } else {
-                                    self.offset = self.openOffset
-                                }
-                            }) {
-                                VStack {
-                                    Image(systemName: "message.fill")
-                                    Text("チャットを\n見てみる")
-                                }
-                            }.buttonStyle(BlueButtonStyle()).frame(width: 160, height: 100)
-                        }
-                    }.onAppear{
-                        if let index = post_name.firstIndex(of: select_list){
-                            selected_get_number = index
-                        }
+            // メインコンテンツ
+            VStack{
+                HStack{
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        Text("戻る")
                     }
-                    .background(Color.white)
-                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
-                    // スライドメニューがでてきたらメインコンテンツをグレイアウトします
-                    Color.gray.opacity(
-                        Double((self.closeOffset - self.offset)/self.closeOffset) - 0.4
-                    )
-                    // スライドメニュー
-                    chat_View(selected_get_number2: $selected_get_number)
-                        .background(Color.white)
-                        .frame(width: geometry.size.width * 0.7)
-                    // 最初に画面のオフセットの値をスライドメニュー分マイナスします。
-                        .onAppear(perform: {
-                            self.offset = geometry.size.width
-                            self.closeOffset = self.offset
-                            self.openOffset = geometry.size.width - geometry.size.width * 0.7
-                        })
-                        .offset(x: self.offset)
-                    // スライドのアニメーションを設定します
-                        .animation(.default)
+                    .padding()
+                    .accentColor(Color.white)
+                    .background(Color.blue)
+                    .cornerRadius(26)
+                    Spacer()
                 }
-                // ジェスチャーに関する実装をします
-                // スワイプのしきい値を設定してユーザの思わぬメニューの出現を防ぎます
-                .gesture(DragGesture(minimumDistance: 5)
-                    .onChanged{ value in
-                        // オフセットの値(メニューの位置)をスワイプした距離に応じて狭めていきます
-                        if (self.offset > self.openOffset) {
-                            self.offset = self.closeOffset + value.translation.width
-                        }
+                Spacer()
+                Text("\(post_title[selected_get_number])").font(.system(size: 50)).fontWeight(.black)
+                VStack{
+                    Text("\(post_content[selected_get_number])").font(.system(size: 20)).fontWeight(.black)
+                }
+                .frame(width: 350, height: 400)
+                .padding()
+                .border(Color.gray, width: 1)
+                HStack{
+                    VStack{
+                        Text("実際の画像").fontWeight(.black)
+                        Image("")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 200, height: 180)
+                            .cornerRadius(0)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 4))
                     }
-                    .onEnded { value in
-                        // value.translation.widthを使っても書けます
-                        // 移動した距離がマイナス方向だったらメニューを開く
-                        if (value.translation.width < 0) {
-                            self.offset = self.openOffset
-                        } else {
+                    Button(action: {
+                        if (self.offset == self.openOffset) {
                             self.offset = self.closeOffset
+                        } else {
+                            self.offset = self.openOffset
                         }
-                    }
-                )
+                    }) {
+                        VStack {
+                            Image(systemName: "message.fill")
+                            Text("チャットを\n見てみる")
+                        }
+                    }.buttonStyle(BlueButtonStyle()).frame(width: 160, height: 100)
+                }
+            }.onAppear{
+                if let index = post_name.firstIndex(of: select_list){
+                    selected_get_number = index
+                }
             }
         }.navigationBarBackButtonHidden(true)
-    }
-}
-struct chat_View: View{
-    @Binding var selected_get_number2: Int
-    @State var user_text_enter = ""
-    @State var post_chat_view = post_chat
-    
-    var body: some View{
-        VStack{
-            Text("チャット内容").font(.system(size: 40)).fontWeight(.black)
-            Text(post_chat_view[selected_get_number2]?.joined(separator: "\n") ?? "").font(.title).fontWeight(.black)
-            Spacer()
-            VStack(alignment: .leading) {
-                HStack {
-                    TextField("返信する", text: $user_text_enter)
-                    Button(action: {
-                        post_chat_view[selected_get_number2]?.append(user_text_enter)
-                        post_chat[selected_get_number2]?.append(user_text_enter)
-                    }){
-                        Image(systemName: "arrow.up.circle").resizable().frame(width: 50, height: 50).foregroundColor(Color.green)
-                    }
-                }
-                .textFieldStyle(OvalTextFieldStyle())
-            }.padding()
-        }.background(Color.white).frame(width: 300, height: 750)
     }
 }
 
